aaf2796914c296116faf713d0cefeb842ec08dad
saikrishna488 <20p61a05h0@vbithyd.ac.in>
2023-04-08 13:50:09 +0530

    first commit

---
+++ routes/dict.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/:word', async (req,res)=>{
+    if(req.params.word){
+        try{
+            let ress = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+req.params.word);
+            let data = ress.data;
+
+            let m = data[0].meanings[0].definitions[0].definition;
+            res.status(200).json({meaning:m})
+
+        }
+        catch(e){
+            res.status(400).json({msg:"check the word you typed"})
+        }
+        
+        
+    }
+    
+})
+
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 2000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/dict',require('./routes/dict'))
+app.get('/*',(req,res)=>{
+    res.status(400).json({msg:"Check your url \"/dict/<word>\" is correct url"});
+})
+
+app.listen(port, ()=> console.log("server started on port"+2000));
+
