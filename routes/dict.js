84c43b80b672b6df59326dd259a936a1e5d4a99e
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2023-04-20 15:28:13 +0530

    Update dict.js

--- routes/dict.js
+++ routes/dict.js
@@ -4,22 +4,26 @@ const axios = require('axios');
 
 router.get('/:word', async (req,res)=>{
     if(req.params.word){
-        try{
-            let ress = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+req.params.word);
-            let data = ress.data;
-
-            let m = data[0].meanings[0].definitions[0].definition;
-            res.status(200).json({meaning:m})
-
+        if(req.params.word == 'deekshitha'){
+            res.status(200).json({meaning:"Good girl , goddess, Forgives everyone including h0"});
         }
-        catch(e){
-            res.status(400).json({msg:"check the word you typed"})
+        else{
+                try{
+                    let ress = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+req.params.word);
+                    let data = ress.data;
+
+                    let m = data[0].meanings[0].definitions[0].definition;
+                    res.status(200).json({meaning:m});
+
+                }
+                catch(e){
+                    res.status(400).json({msg:"check the word you typed"});
+                }
         }
-        
         
     }
     
-})
+});
 
 
 module.exports = router
